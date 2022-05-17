94307fa969b4f9bc214f1850d730e41982f98e38
cirleioliveira <shirley_1979_5@hotmail.com>
2022-05-17 13:02:51 -0400

    First commit Intelly

---
+++ Módulo II - Setup/primeiro-programa-IntelliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato; // Sift+Atl+o para limpar o import que esta sem uso.
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) { //main + Ctrl+espaço
+       Gato gato = new Gato() // fazer import Alt+enter
+        System.out.println(gato);
+
+
+        /*int a = 5;
+       int b = 3;
+        System.out.println("Hello World " + (a+b));*/ // sout + Ctrl+espaço
+
+        // TECLAS DE ATALHO
+        //Ctrl + Shift + f10 para compilar.
+        // no "Eclipse" Selecione o bloco presione as teclas Ctrl+shir+c
+        // Selecione o bloco presione as teclas Ctrl+shir+/ ou Ctrl+/
+        //relatorar ex. classe Shitf+F6 "primeiro-programa"
+        // editor de texto shift+F12
+        // ordenar alinhamento Eclipse Ctrl+shift+6
+        // ordenar alinhamento Eclipse Ctrl+Alt+6
+        // duplicar linha ctrl+d
+        // apagar linha ctrl+y
+        // trocar linha ou bloco de lugar Ctrl+sift + seta cimna/baixo
+        // www.basef.com.br/index.php/Atalhos_do_IntelliJ-Idea
+    }
+}
