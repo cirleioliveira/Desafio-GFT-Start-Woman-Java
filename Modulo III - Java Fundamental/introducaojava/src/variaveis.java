5134df46e709138e0198a990ab14adc16f093297
cirleioliveira <shirley_1979_5@hotmail.com>
2022-05-18 12:48:37 -0400

    variaveis java

---
+++ Modulo III - Java Fundamental/introducaojava/src/variaveis.java
@@ -0,0 +1,18 @@
+public class variaveis {
+    public static void main(String[] args) {
+        int i;
+        int I;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+    }
+}
