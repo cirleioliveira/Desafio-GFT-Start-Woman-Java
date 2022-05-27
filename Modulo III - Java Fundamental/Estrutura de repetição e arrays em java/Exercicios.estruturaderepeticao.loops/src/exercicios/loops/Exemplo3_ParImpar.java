8d79c568b7c2cfbb51556fd97db8f2c1152409a1
Cirlei Oliveira <shirley_1979_5@hotmail.com>
2022-05-27 16:35:51 -0400

    Exemplo Estr.repetição par/impar

---
+++ Modulo III - Java Fundamental/Estrutura de repetição e arrays em java/Exercicios.estruturaderepeticao.loops/src/exercicios/loops/Exemplo3_ParImpar.java
@@ -0,0 +1,34 @@
+package exercicios.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+public class Exemplo3_ParImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int par = 0, impar = 0;
+        System.out.println("Digite a quantidade de numero: ");
+        int qtdnumero = scan.nextInt();
+
+        int contador = 0;
+
+        do {
+            System.out.println("Digite um numero ");
+            int numero = scan.nextInt();
+
+            if (numero % 2 == 0)
+                par++;
+            else impar++;
+
+        contador++;
+        }while (contador < qtdnumero);
+        System.out.println("Quantidade par: " + par);
+        System.out.println("Quantidade impar: " + impar);
+
+    }
+}
