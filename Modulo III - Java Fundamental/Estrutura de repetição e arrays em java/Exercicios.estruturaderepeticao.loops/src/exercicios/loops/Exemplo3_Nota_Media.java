a1c3babb78f19d83354aa8f737eb057d3e76cbbc
Cirlei Oliveira <shirley_1979_5@hotmail.com>
2022-05-27 15:24:23 -0400

    Exemplo Estrutura de repetição Media Maior

---
+++ Modulo III - Java Fundamental/Estrutura de repetição e arrays em java/Exercicios.estruturaderepeticao.loops/src/exercicios/loops/Exemplo3_Nota_Media.java
@@ -0,0 +1,30 @@
+package exercicios.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que leia 5 números
+e informe o maior número
+e a média desses números.
+*/
+public class Exemplo3_Nota_Media {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int soma = 0, contador = 0, maior = 0;
+
+        do {
+        System.out.println("Digite uma nota: ");
+        int nota = scan.nextInt();
+        soma = soma + nota;
+
+        if (nota > maior)
+            maior = nota;
+
+        contador++;
+        }while (contador<3);
+
+        System.out.println("Media: " + soma/contador);
+        System.out.println("Maior: " + maior);
+    }
+}
