453502d9ca3a93eddab97f7c70b12fab8b10aa29
Cirlei Oliveira <shirley_1979_5@hotmail.com>
2022-05-27 14:57:59 -0400

    Exemplo Estrutura de repetição nota

---
+++ Modulo III - Java Fundamental/Estrutura de repetição e arrays em java/Exercicios.estruturaderepeticao.loops/src/exercicios/loops/Exemplo2_Nota.java
@@ -0,0 +1,27 @@
+package exercicios.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+*/
+public class Exemplo2_Nota {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira uma nota: ");
+        int nota = scan.nextInt();
+
+        while (nota < 0 | nota >= 10){
+            System.out.println("Nota invalida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+    }
+
+
+
+}
